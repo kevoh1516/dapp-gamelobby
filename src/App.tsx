2fda9d6eaa17e3e959bad1eef7ae5e6e5258fa27
Kevin Oh <kevoh1516@gmail.com>
2022-05-20 16:28:46 -0400

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Grid, Select } from '@mui/material';
+import { Grid, Select, Typography, Box } from '@mui/material';
 import PlayerCard from './components/PlayerCard';
 import './App.css';
 import { createContext, useState, useContext, useMemo } from 'react';
@@ -13,6 +13,11 @@ interface ContextState {
 
 export const ColorContext = createContext({} as ContextState);
 
+const gridStyle = {
+  display: 'flex', 
+  justifyContent: 'center'
+};
+
 function App() {
   const [colors, setColors] = useState({redAvail: true, blueAvail: true, yellowAvail: true, greenAvail: true});
   const value = useMemo(
@@ -22,22 +27,23 @@ function App() {
 
   return (
     <ColorContext.Provider value={value}>
-      <div className="App">
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} alignItems="center">
-          <Grid item xs={6}>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant='h1' padding={10}>Game Lobby</Typography>
+        <Grid container rowSpacing={15} columnSpacing={20} alignItems="center" paddingX={30}>
+          <Grid item xs={6} sx={gridStyle}>
             <PlayerCard playerNum={1} />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={gridStyle}>
             <PlayerCard playerNum={2} />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={gridStyle}>
             <PlayerCard playerNum={3} />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={gridStyle}>
             <PlayerCard playerNum={4} />
           </Grid>
         </Grid>
-      </div>
+      </Box>
     </ColorContext.Provider>
   );
 }
--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -6,10 +6,12 @@ import {
   MenuItem, 
   SelectChangeEvent, 
   FormControl, 
-  InputLabel 
+  InputLabel,
+  Paper
 } from '@mui/material';
 import { ColorContext } from '../App'
 import { createContext, useState, useContext, useMemo } from 'react';
+import { flexbox } from '@mui/system';
 
 interface PlayerProp {
   playerNum: number;
@@ -18,9 +20,11 @@ interface PlayerProp {
 const PlayerCard = (props: PlayerProp) => {
   const { colors, setColors } = useContext(ColorContext);
   const [color, setColor] = useState('');
+  const [cardColor, setCardColor] = useState('#fff');
 
   const handleChange = (event: SelectChangeEvent) => {
     if (event.target.value === 'red') {
+      setCardColor('#FFA49E');
       setColors((prevState: any) => {
         return {
           ...prevState,
@@ -30,6 +34,7 @@ const PlayerCard = (props: PlayerProp) => {
     }
 
     if (event.target.value === 'yellow') {
+      setCardColor('#F5F184');
       setColors((prevState: any) => {
         return {
           ...prevState,
@@ -39,6 +44,7 @@ const PlayerCard = (props: PlayerProp) => {
     }
 
     if (event.target.value === 'blue') {
+      setCardColor('#7E7CE6');
       setColors((prevState: any) => {
         return {
           ...prevState,
@@ -48,6 +54,7 @@ const PlayerCard = (props: PlayerProp) => {
     }
 
     if (event.target.value === 'green') {
+      setCardColor('#7DF588');
       setColors((prevState: any) => {
         return {
           ...prevState,
@@ -95,25 +102,42 @@ const PlayerCard = (props: PlayerProp) => {
   };
 
   return (
-    <Container>
-      <Typography variant="h1">Player {props.playerNum}</Typography>
+    <Paper 
+      sx={{
+        width: '100%', 
+        height: '100%', 
+        padding: 5,
+        backgroundColor: cardColor,
+      }}
+    >
+      <Container 
+        sx={{
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          justifyContent: 'center',
+          height: '100%'
+        }}
+      >
+        <Typography variant="h4">Player {props.playerNum}</Typography>
 
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Color</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={color}
-          label="Color"
-          onChange={handleChange}
-        >
-          <MenuItem value={'red'} disabled={!colors.redAvail}>Red</MenuItem>
-          <MenuItem value={'yellow'} disabled={!colors.yellowAvail}>Yellow</MenuItem>
-          <MenuItem value={'green'} disabled={!colors.greenAvail}>Green</MenuItem>
-          <MenuItem value={'blue'} disabled={!colors.blueAvail}>Blue</MenuItem>
-        </Select>
-      </FormControl>
-    </Container>
+        <FormControl fullWidth sx={{margin: 1}}>
+          <InputLabel id="demo-simple-select-label">Color</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={color}
+            label="Color"
+            onChange={handleChange}
+          >
+            <MenuItem value={'red'} disabled={!colors.redAvail}>Red</MenuItem>
+            <MenuItem value={'yellow'} disabled={!colors.yellowAvail}>Yellow</MenuItem>
+            <MenuItem value={'green'} disabled={!colors.greenAvail}>Green</MenuItem>
+            <MenuItem value={'blue'} disabled={!colors.blueAvail}>Blue</MenuItem>
+          </Select>
+        </FormControl>
+      </Container>
+    </Paper>
   )
 }
 
