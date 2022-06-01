335e4d532fdde5bb6cb2dd7dccc5045aa01a3d3d
Kevin Oh <kevoh1516@gmail.com>
2022-06-01 13:28:01 -0400

    cleanup prisma

--- prisma/migrations/20220601172012_init/migration.sql
+++ prisma/migrations/20220601172012_init/migration.sql
@@ -5,6 +5,10 @@ CREATE TABLE "Color" (
     "p2" TEXT NOT NULL,
     "p3" TEXT NOT NULL,
     "p4" TEXT NOT NULL,
+    "red" BOOLEAN NOT NULL,
+    "blue" BOOLEAN NOT NULL,
+    "yellow" BOOLEAN NOT NULL,
+    "green" BOOLEAN NOT NULL,
 
     CONSTRAINT "Color_pkey" PRIMARY KEY ("id")
 );
